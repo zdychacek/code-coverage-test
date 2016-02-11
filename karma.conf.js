e7508812f69996a2ca30e35e3e57da2edbba7a1a
Ondrej Zdych <zdych.ondrej@gmail.com>
2016-02-11 22:17:46 +0100

    refactor: es6ify karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,45 +1,36 @@
-var path = require('path');
-var globSync = require('glob').sync;
+import path from 'path';
+import { sync as globSync} from 'glob';
 
-module.exports = function(config) {
-  config.set({
-    frameworks: [ 'browserify', 'jasmine' ],
-    files: [
-      'build/vendor.js',
-      'build/app.js',
-      'src/*.spec.js',
-    ],
-    reporters: [ 'progress', 'coverage' ],
-    preprocessors: {
-      'src/*.spec.js': [ 'browserify' ],
-    },
-    plugins: [
-      'karma-jasmine',
-      'karma-browserify',
-      'karma-phantomjs-launcher',
-      'karma-chrome-launcher',
-      'karma-coverage'
-    ],
-    browsers: [ 'PhantomJS' ],
-    coverageReporter: {
-      reporters: [
-        { type: 'text' },
-        { type : 'html', dir: 'coverage/' },
-      ]
-    },
-    logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browserify: {
-      debug: true,
-      transform: [ 'babelify' ],
-      configure: function (bundler) {
-        bundler.on('prebundle', function () {
-          globSync('./src/**/!(*.spec).js').forEach(function (file) {
-            bundler.external(file);
-          });
-        });
-      }
-    },
-    singleRun: true
-  });
+module.exports = (config) => {
+	config.set({
+		frameworks: [ 'browserify', 'jasmine' ],
+		files: [
+			'build/vendor.js',
+			'build/app.js',
+			'src/*.spec.js',
+		],
+		preprocessors: {
+			'src/*.spec.js': [ 'browserify' ],
+		},
+		reporters: [ 'progress', 'coverage' ],
+		browsers: [ 'PhantomJS' ],
+		coverageReporter: {
+			reporters: [
+				{ type: 'text' },
+				{ type : 'html', dir: 'coverage/' },
+			]
+		},
+		browserify: {
+			debug: true,
+			transform: [ 'babelify' ],
+			configure (bundler) {
+				bundler.on('prebundle', () => {
+					globSync('./src/**/!(*.spec).js').reduce((bundler, file) => bundler.external(file), bundler);
+				});
+			}
+		},
+		logLevel: config.LOG_INFO,
+		autoWatch: false,
+		singleRun: true
+	});
 };
